708ac45a8f2a007c81878d9dc20b5ce293481877
jolisdegats <szwarc.julie@gmail.com>
2020-11-02 20:26:57 +0100

    logos external links

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,6 +1,9 @@
 import React from "react";
 import walkingGif from "../img/gif-anime-julie-marche.gif";
 import { LoadImage } from "../components/LoadImage";
+import sensCritiqueLogo from "../img/icons/sensCritiqueLogo.jpg";
+import githubLogo from "../img/icons/githubLogo.png";
+import linkedinLogo from "../img/icons/linkedinLogo.png";
 
 const Contact = () => {
   return (
@@ -15,21 +18,21 @@ const Contact = () => {
         <h2>Ou retrouvez moi sur :</h2>
         <div className="contactSites">
           <img
-            src="https://www.iconfinder.com/data/icons/logotypes/32/square-linkedin-512.png"
+            src={linkedinLogo}
             alt="Linkedin"
             onClick={() =>
               window.open("https://www.linkedin.com/in/julieszwarc", "_blank")
             }
           />
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png"
+            src={githubLogo}
             alt="Github"
             onClick={() =>
               window.open("https://github.com/jolisdegats", "_blank")
             }
           />
           <img
-            src="https://pbs.twimg.com/profile_images/905010874729066496/lJgq0y4u.jpg"
+            src={sensCritiqueLogo}
             alt="SensCritique"
             onClick={() =>
               window.open("https://www.senscritique.com/Jolisdegats", "_blank")
