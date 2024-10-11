fe2de559cdcabd7c91f3e65c47c387f909331f71
Julie <7164072+jolisdegats@users.noreply.github.com>
2024-10-12 01:51:22 +0200

    coming soon

--- src/App.js
+++ src/App.js
@@ -1,74 +1,19 @@
-import React, { useEffect, useState, Suspense, lazy } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from "react";
 import "./styles/style.css";
-import Header from "./components/Header";
-import Loader from "./components/Loader";
-import { createClient } from "contentful";
-// import About from "./containers/About";
-// import Contact from "./containers/Contact";
-// import Homepage from "./containers/Homepage";
-// import Projects from "./containers/Projects";
-// import UniqueProject from "./containers/UniqueProject";
-
-console.log(process.env.REACT_APP_CONTENTFUL_TOKEN);
-export const client = createClient({
-  space: process.env.REACT_APP_CONTENTFUL_SPACEID,
-  accessToken: process.env.REACT_APP_CONTENTFUL_TOKEN,
-});
+import avatar from "./img/avatar.png";
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const About = lazy(() => import("./containers/About"));
-  const Contact = lazy(() => import("./containers/Contact"));
-  const Homepage = lazy(() => import("./containers/Homepage"));
-  const Projects = lazy(() => import("./containers/Projects"));
-  const UniqueProject = lazy(() => import("./containers/UniqueProject"));
-
-  // TEST LOADING
-  document.body.classList.add("js-loading");
-  window.addEventListener("load", showPage);
-  function showPage() {
-    document.body.classList.remove("js-loading");
-  }
-
-  useEffect(() => {
-    setIsLoading(false);
-  }, []);
-
   return (
     <div className="App">
-      <Router>
-        {isLoading ? (
-          <Loader></Loader>
-        ) : (
-          <>
-            <Header></Header>
-            <Suspense fallback={<Loader></Loader>}>
-              <Switch>
-                <Route path="/a-propos">
-                  <About></About>
-                </Route>
-                <Route path="/loader">
-                  <Loader></Loader>
-                </Route>
-                <Route path="/projects/:idName">
-                  <UniqueProject></UniqueProject>
-                </Route>
-                <Route path="/projects">
-                  <Projects></Projects>
-                </Route>
-                <Route path="/contact">
-                  <Contact></Contact>
-                </Route>
-                <Route path="/">
-                  <Homepage></Homepage>
-                </Route>
-              </Switch>
-            </Suspense>
-            <footer></footer>
-          </>
-        )}
-      </Router>
+      <div className="coming-soon">
+        <img
+          src={avatar}
+          alt="Anime-style avatar"
+          className="coming-soon__background"
+        />
+        <h1 className="coming-soon__text">Will be back soon...</h1>
+        <div className="coming-soon__glitch-effect"></div>
+      </div>
     </div>
   );
 };
